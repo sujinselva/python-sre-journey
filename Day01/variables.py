d8a474a1a2c3c1c24236eedc9807f9b54b81084c
Sujin Selva <Sujin_Selva@genpt.com>
2026-07-23 10:17:23 -0600

    check workflow

--- Day01/variables.py
+++ Day01/variables.py
@@ -1,7 +1,9 @@
-name = "Sujin"
-role = "SRE"
-experience = 14
+site = "Syracuse"
+environment = "Production"
+cpu_usage = 82
+memory_usage = 67
 
-print(name)
-print(role)
-print(experience)
+print("Site:", site)
+print("Environment:", environment)
+print("CPU Usage:", cpu_usage, "%")
+print("Memory Usage:", memory_usage, "%")
